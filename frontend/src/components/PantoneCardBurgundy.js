10e463436abf10ce6d62f95703d831d260bdc2b7
NicciVanZyl <251023@virtualwindow.co.za>
2026-05-08 15:23:52 +0200

    Added all previously done work onto new Repo

--- frontend/src/components/PantoneCardBurgundy.js
+++ frontend/src/components/PantoneCardBurgundy.js
@@ -6,7 +6,7 @@ function PantoneCard() {
     return (
         <div className="pantoneComponent">
             <div className="pantoneCard">
-                <div className="swatchColour lightBlue"></div>
+                <div className="swatchColour burgundy"></div>
                 <div className="pantoneName">
                     <h3>Colour Name</h3>
                     <h4>Pantone™</h4>
