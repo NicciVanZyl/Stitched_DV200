f23b5ee9a00d154132cb6c568b4555060fffad93
NicciVanZyl <251023@virtualwindow.co.za>
2026-05-12 13:21:55 +0200

    Started on PDP

--- frontend/src/pages/PDP.js
+++ frontend/src/pages/PDP.js
@@ -8,14 +8,31 @@ import placeholderImage from '../images/product image placeholder.png';
 
 function ProductDetails() {
     return(
-      <Container className=''>
-        <h1>PDP Page</h1>
+      <Container className='productDetails' >
         <Row>
-          <Col lg={6} md={12}>
+          <Col lg={6} md={12} className='pdImage'>
             <img src={placeholderImage}></img>
           </Col>
-          <Col lg={6} md={12}>
-            
+          <Col lg={6} md={12} className='pdDetails'>
+            <Stack gap={5}>
+              <Stack gap={4}>
+              <div>
+                <h1>Product Name</h1>
+                <h2>Product Price</h2>
+              </div>
+              <div>
+                <h3>Seller Name</h3>
+                <h3>Seller Rating</h3>
+              </div>
+              </Stack>
+              <p>Product details: description & size Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet orci neque. Aenean non sem ut massa interdum ultrices quis posuere purus. Ut ullamcorper magna non nunc fringilla, id sodales turpis laoreet. Mauris rutrum risus tristique, cursus eros in, tincidunt odio. Nunc non gravida purus. Morbi pretium arcu eu commodo tempor. Praesent euismod rutrum lorem, at tincidunt magna laoreet id.
+              </p>
+              <Stack direction='horizontal' gap={4}>
+                <button>Add to cart</button>
+                <button>Like</button>
+                <button>Flag</button>
+              </Stack>
+            </Stack>
           </Col>
         </Row>
       </Container>  
