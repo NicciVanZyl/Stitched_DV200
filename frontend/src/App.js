441d07bfa8388f114e29d8a22c67032a9eb4197b
LeandreNel <251139@virtualwindow.co.za>
2026-05-13 19:13:53 +0200

    Personalprofile
    this made me sad

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Navigation from "./components/navbar";
 import Home from "./pages/Home";
@@ -8,24 +8,24 @@ import Cart from "./pages/Cart";
 import ProductDetails from "./pages/PDP";
 import ProductListing from "./pages/PLP";
 import Profile from "./pages/Profile";
+import PersonalProfile from "./pages/personalProfile";
 import Admin from "./pages/Admin";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Navigation></Navigation>
       <Routes>
-        <Route path='/' element={<Login />}></Route>
-        <Route path='/Home' element={<Home />}></Route>
-        <Route path='/Cart' element={<Cart />}></Route>
-        <Route path='/ProductDetails' element={<ProductDetails />}></Route>
-        <Route path='/ProductListing' element={<ProductListing />}></Route>
-        <Route path='/Profile' element={<Profile />}></Route>
-        <Route path='/Admin' element={<Admin />}></Route>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/Home" element={<Home />}></Route>
+        <Route path="/Cart" element={<Cart />}></Route>
+        <Route path="/ProductDetails" element={<ProductDetails />}></Route>
+        <Route path="/ProductListing" element={<ProductListing />}></Route>
+        <Route path="/Profile" element={<Profile />}></Route>
+        <Route path="/personalProfile" element={<PersonalProfile />}></Route>
+        <Route path="/Admin" element={<Admin />}></Route>
       </Routes>
-
     </BrowserRouter>
   );
 }
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,37 +1,69 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { List } from 'react-bootstrap-icons';
-import StitchedRedLogo from '../images/StitchedRedLogo.png';
-import CartIcon from '../images/CartIcon.png';
-import ProfileIcon from '../images/ProfileIcon.png';
-import WishlistIcon from '../images/WishlistIcon.png';
-import FlagIcon from '../images/FlagIcon.png';
+import { List } from "react-bootstrap-icons";
+import StitchedRedLogo from "../images/StitchedRedLogo.png";
+import CartIcon from "../images/CartIcon.png";
+import ProfileIcon from "../images/ProfileIcon.png";
+import WishlistIcon from "../images/WishlistIcon.png";
+import FlagIcon from "../images/FlagIcon.png";
 
 function Navigation() {
   return (
-    <Navbar expand="lg" className="fixed-top navBackground" variant='light'>
+    <Navbar
+      expand="lg"
+      className="navBackground navbar"
+      margin-top="8rem"
+      position="fixed"
+      // variant="light"
+    >
       <Container>
-        <Navbar.Brand>
-          <Link className='link Logo' to='/Home'>
-            <img src={StitchedRedLogo} alt='Stitched Logo' className='navbarLogo'/>
-          </Link>
-        </Navbar.Brand>
+        <Navbar.Brand></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <List color="white" size={30} />
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Login</Nav.Link>
-            <Nav.Link as={Link} to="/Home">Home</Nav.Link>
-            <Nav.Link as={Link} to="/ProductDetails">Product Details</Nav.Link>
-            <Nav.Link as={Link} to="/ProductListing">Product Listings</Nav.Link>
-            <Nav.Link as={Link} to="/Flag"><img src={FlagIcon} alt='Profile Icon' className='navIcons'/></Nav.Link>
-            <Nav.Link as={Link} to="/Wishlist"><img src={WishlistIcon} alt='Cart Icon' className='navIcons'/></Nav.Link>
-            <Nav.Link as={Link} to="/Cart"><img src={CartIcon} alt='Cart Icon' className='navIcons'/></Nav.Link>
-            <Nav.Link as={Link} to="/Profile"><img src={ProfileIcon} alt='Profile Icon' className='navIcons'/></Nav.Link>
-            <Nav.Link as={Link} to="/Admin">Admin</Nav.Link>
+            <Nav.Link as={Link} to="/Home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/ProductListing">
+              Shop
+            </Nav.Link>
+            <Nav.Link as={Link} to="/personalProfile">
+              Personal Profile
+            </Nav.Link>
+            <Link className="link Logo" to="/Home">
+              <img
+                src={StitchedRedLogo}
+                alt="Stitched Logo"
+                className="navbarLogo"
+              />
+            </Link>
+            <Nav.Link as={Link} to="/">
+              Login
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/ProductDetails">
+              Product Details
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/Flag">
+              <img src={FlagIcon} alt="Profile Icon" className="navIcons" />
+            </Nav.Link>
+            <Nav.Link as={Link} to="/Wishlist">
+              <img src={WishlistIcon} alt="Cart Icon" className="navIcons" />
+            </Nav.Link>
+            <Nav.Link as={Link} to="/Cart">
+              <img src={CartIcon} alt="Cart Icon" className="navIcons" />
+            </Nav.Link>
+            <Nav.Link as={Link} to="/Profile">
+              <img src={ProfileIcon} alt="Profile Icon" className="navIcons" />
+            </Nav.Link>
+            <Nav.Link as={Link} to="/Admin">
+              Admin
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
