0aeb2cced111d762c0377bf8fc976310fc10191e
NicciVanZyl <251023@virtualwindow.co.za>
2026-05-13 19:31:08 +0200

    Cart Css

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import { Trash } from "react-bootstrap-icons";
+import './CartAndAndmin.css';
 
 function Cart() {
   return (
