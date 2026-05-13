ce18ef1ce9e10608cbaed616b5a29a5633c87007
LeandreNel <251139@virtualwindow.co.za>
2026-05-13 14:20:32 +0200

    idk

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -1,12 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const commentSchema = new mongoose.Schema(
-    {
-        commentBody: { type: String, required: true },
-        customerID: { type: String, required: true },
-        rating: { type: Float, required: true }
-    },
-);
+const commentSchema = new mongoose.Schema({
+  commentBody: { type: String, required: true },
+  customerID: { type: String, required: true },
+  rating: { type: String, required: true },
+});
 
-const Comment = mongoose.model('Comment', commentSchema);
+const Comment = mongoose.model("Comment", commentSchema);
 module.exports = Comment;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
-
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -12,16 +12,17 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("API is running");
+  res.send("API is running");
 });
 
 // Connect MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.listen(5000, () => {
-    console.log("Server running on port 5000");
+  console.log("Server running on port 5000");
 });
 
 const commentsRoutes = require("./routes/commentsRoute");
