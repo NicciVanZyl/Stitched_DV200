261e31eb5b985bab6a930405d542adc97b5d857f
LeandreNel <251139@virtualwindow.co.za>
2026-06-01 18:18:37 +0200

    push

--- backend/models/listing.js
+++ backend/models/listing.js
@@ -1,19 +1,23 @@
 const mongoose = require("mongoose");
 
 const listingSchema = new mongoose.Schema(
-    {
-        name: { type: String, required: true, trim: true },
-        price: { type: String, required: true },
-        description: { type: String, required: true, maxlength: 800, trim: true },
-        size: { type: String, required: true },
-        imageUrl: {type: String, required: true},
-        isActive: { type: Boolean, default: false },
-        customerID: { type: String, required: true },
-        isSold:{type: Boolean, default: false },
-        postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-        likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  {
+    name: { type: String, required: true, trim: true },
+    price: { type: String, required: true },
+    description: { type: String, required: true, maxlength: 800, trim: true },
+    size: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    isActive: { type: Boolean, default: false },
+    customerID: { type: String, required: false },
+    isSold: { type: Boolean, default: false },
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    { timestamps: true}
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true },
 );
 
 const Listing = mongoose.model("Listing", listingSchema);
